5337750c5cd661f44abc673e0c574f6a0caeccd4
hocineimail <hocineismail75@gmail.com>
2018-10-14 17:31:07 +0100

    add the foote of homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  root 'homes#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :datebookings do
    resources :timetebookings
   end
 
-  root "datebookings#index"
+ 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
