75b4d6f7a1c27aef34338e2f34778a03d39395d3
hocineimail <hocineismail75@gmail.com>
2018-10-14 16:39:58 +0100

    delete function destroy

--- app/controllers/timetebookings_controller.rb
+++ app/controllers/timetebookings_controller.rb
@@ -4,26 +4,15 @@ class TimetebookingsController < ApplicationController
 
 
     @datebooking = Datebooking.find(params[:datebooking_id])
-    @zeb = params.require(:timetebooking).permit(:time) 
-    @rep = true
-     
-      if @rep == true
+  
       @timebooking = @datebooking.timetebookings.create(timebooking_params)
       redirect_to datebooking_path(@datebooking)
-     else 
-        redirect_to datebooking_path(@datebooking)
-     end
+     
+    
 	end
 
 
 
-def repa 
-	@datebooking.timetebookings.each do  |n|
-       if n.time ==  @zeb
-            @rep = false
-       end
-      end 
-end
 
 
 
